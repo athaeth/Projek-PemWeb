6ec98e29ba889ec5acc17bdb6451df3f8ad43e54
Evan Wijaya <riffanevan1234@gmail.com>
2025-11-10 13:18:01 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 </main>
 
 <footer class="text-center text-muted mt-5 mb-3">
-  <p>&copy; <?php echo date('Y'); ?> Perpustakaan Pintar. All Rights Reserved.</p>
+  <p>&copy; <?php echo date('Y'); ?> Perpustakaan Pintar.</p>
 </footer>
 
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
